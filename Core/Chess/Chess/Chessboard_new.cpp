6dfdd459e65c7f6a7e55fb50a6b908cdff930b64
Menchcruts <joelarnar@gmail.com>
2025-08-03 19:44:11 +0000

    New Chessboard class, will replace old Chessboard in the future

---
+++ Core/Chess/Chess/Chessboard_new.cpp
@@ -0,0 +1,315 @@
+#include "Chessboard_new.h"
+#include <ranges>
+#include <string>
+#include <algorithm>
+#include <charconv>
+#include <iostream>
+
+void Chess_Rework::Chessboard_New::ResetBoard()
+{
+    // Reset the board to the initial position
+    LoadFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+}
+
+void Chess_Rework::Chessboard_New::PlacePiece(Square sq, Piece piece)
+{
+	if (!is_ok(sq))
+		return;
+
+	RemovePiece(sq);
+
+    m_BoardArray[sq] = piece;
+	m_Bitboards.SetBit(sq, piece);
+}
+
+Chess_Rework::Piece Chess_Rework::Chessboard_New::RemovePiece(Square sq)
+{
+	if ( !is_ok(sq))
+		return Piece::NoPiece;
+
+	Piece piece = m_BoardArray[sq];
+	m_BoardArray[sq] = NoPiece;
+	m_Bitboards.ClearBit(sq, piece);
+	return piece;
+}
+
+Chess_Rework::Piece Chess_Rework::Chessboard_New::GetPiece(Square sq) const
+{
+    if (!is_ok(sq))
+		return NoPiece;
+    return m_BoardArray[sq];
+}
+
+/* Takes a string representing a square in normal chess notation, i.e. a5, b7 and so on, and converts it into a Square enum. */
+static Chess_Rework::Square square_from_string(const std::string& sq_as_str)
+{
+    if (sq_as_str.size() != 2)
+        return Chess_Rework::NoSquare;
+    char file = sq_as_str[0];
+    char rank = sq_as_str[1];
+    if (file < 'a' || file > 'h' || rank < '1' || rank > '8')
+        return Chess_Rework::NoSquare;
+    return Chess_Rework::Square((rank - '1') * 8 + (file - 'a'));
+}
+
+void Chess_Rework::Chessboard_New::MakeMove(Move move)
+{    
+    // TODO: Implement null move logic
+    
+    Square from_sq = from_square(move);
+	Square to_sq = to_square(move);
+	MoveFlag flag = move_flag(move);
+
+    std::cout << "Making move: " << move << " from " << from_sq << " to " << to_sq << " with flag: " << flag << "\n";
+    
+    Piece moving_piece = GetPiece(from_sq);
+	PieceType moving_piece_type = type_of(moving_piece);
+    if (moving_piece == NoPiece)
+		return;
+
+    Square capture_sq;
+	bool is_capture = (flag & MoveFlag::Capture) != MoveFlag::None;
+    if (is_capture && flag == MoveFlag::EnPassant)
+		capture_sq = to_sq + (moving_piece == W_Pawn ? Dir_South : Dir_North);
+    else
+		capture_sq = to_sq;
+
+    Piece captured_piece = GetPiece(capture_sq);
+	PieceType captured_piece_type = type_of(captured_piece);
+
+    m_PrevStates.emplace_back(PrevState{
+        .HalfMoveClock = m_HalfMoveClock,
+        .EP_Square = m_EP_Square,
+        .WhiteCastle = m_WhiteCastle,
+        .BlackCastle = m_BlackCastle,
+        .CapturedPiece = captured_piece
+		});
+
+	m_MoveHistory.emplace_back(move);
+
+    // Update the board and bitboards
+	RemovePiece(from_sq);
+    if (is_capture)
+        RemovePiece(capture_sq);
+	PlacePiece(to_sq, moving_piece);
+
+    // Update castling rights
+	CastlingRights& current_castle  = m_WhiteToMove ? m_WhiteCastle : m_BlackCastle;
+	CastlingRights castleOO         = m_WhiteToMove ? CastlingRights::White_OO : CastlingRights::Black_OO;
+	CastlingRights castleOOO        = m_WhiteToMove ? CastlingRights::White_OOO : CastlingRights::Black_OOO;
+
+	if (moving_piece_type == PieceType::King && current_castle != CastlingRights::No_Castling)
+		current_castle = CastlingRights::No_Castling;   // Remove all castling rights if king moves for the first time
+
+    if (moving_piece_type == PieceType::Rook)
+    {
+		Square OO_rook_sq     = m_WhiteToMove ? Square::SQ_H1 : Square::SQ_H8;
+        Square OOO_rook_sq    = m_WhiteToMove ? Square::SQ_A1 : Square::SQ_A8;
+        
+        if (from_sq == OO_rook_sq && has_castling_rights(current_castle, castleOO))
+            current_castle ^= castleOO; // Remove kingside castling rights
+        
+        else if (from_sq == OOO_rook_sq && has_castling_rights(current_castle, castleOOO))
+			current_castle ^= castleOOO; // Remove queenside castling rights
+    }
+
+	// Update en passant square
+    if (flag == MoveFlag::DoublePawnMove)
+        m_EP_Square = to_sq + (moving_piece == W_Pawn ? Dir_North : Dir_South);
+    else
+        m_EP_Square = NoSquare; // Clear en passant square
+
+    // Handle castling
+    if (flag == MoveFlag::CastleKing)
+    {
+		Square rook_from_sq = (m_WhiteToMove) ? Square::SQ_H1 : Square::SQ_H8;
+		Square rook_to_sq = (m_WhiteToMove) ? Square::SQ_F1 : Square::SQ_F8;
+		PlacePiece(rook_to_sq, GetPiece(rook_from_sq));
+        RemovePiece(rook_from_sq);
+    }
+    else if (flag == MoveFlag::CastleQueen)
+    {
+		Square rook_from_sq = (m_WhiteToMove) ? Square::SQ_A1 : Square::SQ_A8;
+		Square rook_to_sq = (m_WhiteToMove) ? Square::SQ_D1 : Square::SQ_D8;
+        PlacePiece(rook_to_sq, GetPiece(rook_from_sq));
+        RemovePiece(rook_from_sq);
+	}
+
+	// Update clocks and swap turn
+    if (!m_WhiteToMove)
+		++m_FullMoveClock; // Increment full move clock on black move
+
+	if (moving_piece_type == PieceType::Pawn || is_capture)
+        m_HalfMoveClock = 0; // Reset half move clock on pawn move or capture
+    else
+        ++m_HalfMoveClock; // Increment half move clock otherwise
+
+	m_WhiteToMove = !m_WhiteToMove; // Switch turn
+}
+
+void Chess_Rework::Chessboard_New::UnMakeMove(Move move)
+{
+    if (move != m_MoveHistory.back())
+        return; // Future logging
+
+	m_MoveHistory.pop_back();
+	PrevState prev_state = m_PrevStates.back(); m_PrevStates.pop_back();
+
+	Square from_sq = from_square(move);
+	Square to_sq = to_square(move);
+    MoveFlag flag = move_flag(move);
+
+	bool is_capture = (flag & MoveFlag::Capture) != MoveFlag::None;
+
+	Piece moving_piece = GetPiece(to_sq);
+	PieceType moving_piece_type = type_of(moving_piece);
+
+	Piece captured_piece = prev_state.CapturedPiece;
+	PieceType captured_piece_type = type_of(captured_piece);
+
+	m_WhiteCastle = prev_state.WhiteCastle;
+	m_BlackCastle = prev_state.BlackCastle;
+	m_EP_Square = prev_state.EP_Square;
+	m_HalfMoveClock = prev_state.HalfMoveClock;
+	
+    m_WhiteToMove = !m_WhiteToMove; // Switch turn back
+    
+    if (!m_WhiteToMove)
+		--m_FullMoveClock; // Decrement full move clock on black move
+
+    Square capture_sq;
+    if (flag == MoveFlag::EnPassant)
+		capture_sq = to_sq + (moving_piece == W_Pawn ? Dir_South : Dir_North);
+	else
+		capture_sq = to_sq;
+
+	RemovePiece(to_sq);
+    if (is_capture)
+		PlacePiece(capture_sq, captured_piece);
+    PlacePiece(from_sq, moving_piece);
+
+    if (flag == MoveFlag::CastleKing)
+    {
+        Square rook_from_sq = (m_WhiteToMove) ? Square::SQ_F1 : Square::SQ_F8;
+        Square rook_to_sq = (m_WhiteToMove) ? Square::SQ_H1 : Square::SQ_H8;
+        PlacePiece(rook_to_sq, GetPiece(rook_from_sq));
+        RemovePiece(rook_from_sq);
+    }
+    else if (flag == MoveFlag::CastleQueen)
+    {
+        Square rook_from_sq = (m_WhiteToMove) ? Square::SQ_D1 : Square::SQ_D8;
+        Square rook_to_sq = (m_WhiteToMove) ? Square::SQ_A1 : Square::SQ_A8;
+        PlacePiece(rook_to_sq, GetPiece(rook_from_sq));
+		RemovePiece(rook_from_sq);
+    }
+}
+
+void Chess_Rework::Chessboard_New::LoadFEN(const std::string_view& FEN_String)
+{
+	std::vector<std::string> FEN_Tokens;
+    for (auto&& token : FEN_String | std::views::split(' ')) {
+        FEN_Tokens.emplace_back(token.begin(), token.end());
+	}
+
+    if (FEN_Tokens.size() != 6)
+    {
+		LoadFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+		return;
+    }
+
+	std::string BoardString = FEN_Tokens[0];
+	std::string CastleString = FEN_Tokens[2];
+	
+	// Basic metadata
+    m_WhiteToMove = (FEN_Tokens[1] == "w");
+	m_EP_Square = FEN_Tokens[3] == "-" ? NoSquare : square_from_string(FEN_Tokens[3]);
+	
+	m_HalfMoveClock = 0;
+	m_FullMoveClock = 1;
+	std::from_chars(FEN_Tokens[4].data(), FEN_Tokens[4].data() + FEN_Tokens[4].size(), m_HalfMoveClock);
+	std::from_chars(FEN_Tokens[5].data(), FEN_Tokens[5].data() + FEN_Tokens[5].size(), m_FullMoveClock);
+
+
+	// Set castling rights
+	m_WhiteCastle = CastlingRights::No_Castling;
+	m_BlackCastle = CastlingRights::No_Castling;
+
+    for (const auto& castle_char : CastleString)
+    {
+        switch (castle_char)
+        {
+            case 'K': m_WhiteCastle |= CastlingRights::White_OO; break; // White kingside
+            case 'Q': m_WhiteCastle |= CastlingRights::White_OOO; break; // White queenside
+            case 'k': m_BlackCastle |= CastlingRights::Black_OO; break; // Black kingside
+            case 'q': m_BlackCastle |= CastlingRights::Black_OOO; break; // Black queenside
+            default: break; // Ignore any other characters
+        }
+	}
+
+	// Set the board
+    m_PrevStates.clear();
+	m_BoardArray.fill(NoPiece);
+	m_Bitboards = PieceBitboards{}; // Reset all bitboards
+
+    auto split = BoardString | std::views::split('/')
+                             | std::views::transform([](const auto& str) {
+                                return std::string(str.data(), str.size());
+								});
+
+    auto RankStrings = std::vector(split.begin(), split.end());
+    std::reverse(RankStrings.begin(), RankStrings.end());
+
+    Square CurrentSquare = Square::SQ_A1;
+    for (const auto& RankString : RankStrings)
+    {
+		bool IsWhite;
+        for (const auto& Char : RankString)
+        {
+            if ('0' <= Char && Char <= '9')
+            {
+				int skip = Char - '0';
+                CurrentSquare += Square(skip); // Skip squares
+                continue;
+            }
+            else if ('a' <= Char && Char <= 'z') // Char is a black piece
+            {
+				IsWhite = false;
+            }
+            else if ('A' <= Char && Char <= 'Z') // Char is a white piece
+            {
+                IsWhite = true;
+			}
+            else
+            {
+				// Maybe logging
+                continue; // Invalid character, skip
+            }
+
+			Piece NewPiece = NoPiece;
+
+			if (Char == 'k' || Char == 'K')
+				NewPiece = IsWhite ? W_King : B_King;
+			else if (Char == 'q' || Char == 'Q')
+				NewPiece = IsWhite ? W_Queen : B_Queen;
+			else if (Char == 'r' || Char == 'R')
+				NewPiece = IsWhite ? W_Rook : B_Rook;
+			else if (Char == 'b' || Char == 'B')
+				NewPiece = IsWhite ? W_Bishop : B_Bishop;
+			else if (Char == 'n' || Char == 'N')
+				NewPiece = IsWhite ? W_Knight : B_Knight;
+			else if (Char == 'p' || Char == 'P')
+				NewPiece = IsWhite ? W_Pawn : B_Pawn;
+            else { /* Maybe logging */ }
+
+			if (NewPiece != NoPiece)
+                PlacePiece(CurrentSquare, NewPiece);
+
+			CurrentSquare += Square(1); // Move to the next square
+        }
+	}
+}
+
+void Chess_Rework::Chessboard_New::GenerateMoves()
+{
+
+}
---
+++ Core/Chess/Chess/Chessboard_new.h
@@ -0,0 +1,178 @@
+#pragma once
+#include "types.h"
+#include "Bitboards.h"
+#include <array>
+#include <vector>
+#include <string_view>
+#include <unordered_set>
+
+namespace Chess_Rework
+{
+	class Chessboard_New
+	{
+		struct PieceBitboards
+		{
+			Bitboard WKings		= 0;
+			Bitboard BKings		= 0;
+			Bitboard WQueens	= 0;
+			Bitboard BQueens	= 0;
+			Bitboard WRooks		= 0;
+			Bitboard BRooks		= 0;
+			Bitboard WBishops	= 0;
+			Bitboard BBishops	= 0;
+			Bitboard WKnights	= 0;
+			Bitboard BKnights	= 0;
+			Bitboard WPawns		= 0;
+			Bitboard BPawns		= 0;
+
+			void ToggleBit(Square sq, Piece piece)
+			{
+				Bitboard sq_bb = Bitboards::from_sq(sq);
+				switch (piece)
+				{
+					case Piece::W_King:   WKings	^= sq_bb; break;
+					case Piece::B_King:   BKings	^= sq_bb; break;
+					case Piece::W_Queen:  WQueens	^= sq_bb; break;
+					case Piece::B_Queen:  BQueens	^= sq_bb; break;
+					case Piece::W_Rook:   WRooks	^= sq_bb; break;
+					case Piece::B_Rook:   BRooks	^= sq_bb; break;
+					case Piece::W_Bishop: WBishops	^= sq_bb; break;
+					case Piece::B_Bishop: BBishops	^= sq_bb; break;
+					case Piece::W_Knight: WKnights	^= sq_bb; break;
+					case Piece::B_Knight: BKnights	^= sq_bb; break;
+					case Piece::W_Pawn:   WPawns	^= sq_bb; break;
+					case Piece::B_Pawn:   BPawns	^= sq_bb; break;
+					default: break;
+				}
+			}
+			void SetBit(Square sq, Piece piece)
+			{
+				Bitboard sq_bb = Bitboards::from_sq(sq);
+				switch (piece)
+				{
+					case Piece::W_King:   WKings	|= sq_bb; break;
+					case Piece::B_King:   BKings	|= sq_bb; break;
+					case Piece::W_Queen:  WQueens	|= sq_bb; break;
+					case Piece::B_Queen:  BQueens	|= sq_bb; break;
+					case Piece::W_Rook:   WRooks	|= sq_bb; break;
+					case Piece::B_Rook:   BRooks	|= sq_bb; break;
+					case Piece::W_Bishop: WBishops	|= sq_bb; break;
+					case Piece::B_Bishop: BBishops	|= sq_bb; break;
+					case Piece::W_Knight: WKnights	|= sq_bb; break;
+					case Piece::B_Knight: BKnights	|= sq_bb; break;
+					case Piece::W_Pawn:   WPawns	|= sq_bb; break;
+					case Piece::B_Pawn:   BPawns	|= sq_bb; break;
+					default: break;
+				}
+			}
+			void ClearBit(Square sq, Piece piece)
+			{
+				Bitboard sq_bb = Bitboards::from_sq(sq);
+				switch (piece)
+				{
+					case Piece::W_King:   WKings	&= ~sq_bb; break;
+					case Piece::B_King:   BKings	&= ~sq_bb; break;
+					case Piece::W_Queen:  WQueens	&= ~sq_bb; break;
+					case Piece::B_Queen:  BQueens	&= ~sq_bb; break;
+					case Piece::W_Rook:   WRooks	&= ~sq_bb; break;
+					case Piece::B_Rook:   BRooks	&= ~sq_bb; break;
+					case Piece::W_Bishop: WBishops	&= ~sq_bb; break;
+					case Piece::B_Bishop: BBishops	&= ~sq_bb; break;
+					case Piece::W_Knight: WKnights	&= ~sq_bb; break;
+					case Piece::B_Knight: BKnights	&= ~sq_bb; break;
+					case Piece::W_Pawn:   WPawns	&= ~sq_bb; break;
+					case Piece::B_Pawn:   BPawns	&= ~sq_bb; break;
+					default: break;
+				}
+			}
+			
+			Bitboard GetAllPieces() const
+			{
+				return	WKings | WPawns | WKnights | WBishops | WRooks | WQueens | 
+						BKings | BPawns | BKnights | BBishops | BRooks | BQueens;
+			}
+			Bitboard GetPieces(Color c) const
+			{
+				return c == White ? WKings | WPawns | WKnights | WBishops | WRooks | WQueens :
+									BKings | BPawns | BKnights | BBishops | BRooks | BQueens;
+			}
+		};
+
+		struct PrevState
+		{
+			int HalfMoveClock = 0;
+			Square EP_Square = NoSquare;
+			CastlingRights WhiteCastle = CastlingRights::No_Castling;
+			CastlingRights BlackCastle = CastlingRights::No_Castling;
+			Piece CapturedPiece = NoPiece; // Possibly move this to separate vector later
+		};
+
+		template<typename T>
+		static inline constexpr std::vector<T> make_reserved_vector(size_t reserved_space)
+		{
+			std::vector<T> result;
+			result.reserve(reserved_space);
+			return result;
+		}
+
+	public:
+		void MakeMove(Move move);
+		void UnMakeMove(Move move);
+		void LoadFEN(const std::string_view& FEN_String);
+		void ResetBoard();
+		void PlacePiece(Square sq, Piece piece);
+		Piece RemovePiece(Square sq);
+		Piece GetPiece(Square sq) const;
+
+		CastlingRights GetCastlingRights(Color c) const
+		{
+			return c == White ? m_WhiteCastle : m_BlackCastle;
+		}
+		bool IsWhiteToMove() const
+		{
+			return m_WhiteToMove;
+		}
+		Square GetEnPassantSquare() const
+		{
+			return m_EP_Square;
+		}
+		int GetHalfMoveClock() const
+		{
+			return m_HalfMoveClock;
+		}
+		int GetFullMoveClock() const
+		{
+			return m_FullMoveClock;
+		}
+		const PieceBitboards& GetBitboards() const
+		{
+			return m_Bitboards;
+		}
+		const Move& GetLastMove() const
+		{
+			return m_MoveHistory.empty() ? NullMove : m_MoveHistory.back();
+		}
+
+	private:
+		// Piece placements
+		PieceBitboards m_Bitboards;
+		std::array<Piece, 64> m_BoardArray{};
+
+		// Board state
+		std::vector<PrevState> m_PrevStates = make_reserved_vector<PrevState>(128);
+		std::vector<Move> m_MoveHistory = make_reserved_vector<Move>(128);
+
+		int m_HalfMoveClock = 0;
+		int m_FullMoveClock = 1;
+
+		bool m_WhiteToMove = true;
+		Square m_EP_Square = NoSquare;
+		
+		CastlingRights m_WhiteCastle = CastlingRights::No_Castling;
+		CastlingRights m_BlackCastle = CastlingRights::No_Castling;
+
+	private:
+		void GenerateMoves();
+	//	Move* GetMoves();
+	};
+}
