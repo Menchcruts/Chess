daa074f5c25960e9363430219e9e98814ef2dbd0
Menchcruts <joelarnar@gmail.com>
2025-08-23 20:41:26 +0000

    Moved board drawing to separate method, created the promotion window and added better input handling.

--- ChessManager/Rework/Windows/ChessWindow.cpp
+++ ChessManager/Rework/Windows/ChessWindow.cpp
@@ -3,97 +3,42 @@
 #include "Chess/Chessboard_new.h"
 
 #include <iostream>
+#include <format>
 
 ChessWindow::ChessWindow(std::string Name, Chess_Rework::Chessboard_New& board, assets::ImageManager& images) :
 	Window(std::move(Name)), Board(board), Images(images)
 {
-
+	MoveHistory.reserve(8);
 }
 
 void ChessWindow::Draw()
 {
 	namespace Chess = Chess_Rework;
-	using Chess::Piece, Chess::Square, Chess::Bitboards::is_legal;
-	
-	static Piece PieceHeld = Piece::NoPiece;
-	static int SelectedSquare = -1;
-	static bool SelectedAgain = false;
-	static int HoveredSquare = -1;
-	bool any_hovered = false;
+	using Chess::Piece, Chess::Move;
 
-	float CellSide = 100.f;
-	ImVec2 Cellsize = ImVec2(CellSide, CellSide);
-	ImVec2 Cellsize_Half = ImVec2(CellSide/2, CellSide/2);
+	MouseClicked	= ImGui::IsMouseClicked(ImGuiMouseButton_Left);
+	MouseReleased	= ImGui::IsMouseReleased(ImGuiMouseButton_Left);
+	LeftMouseDown	= ImGui::IsMouseDown(ImGuiMouseButton_Left);
 
-	ImGui::Begin(WindowName.c_str());
-	
-	if (ImGui::BeginTable("Chessboard", 8))
-	{
-		for (int column = 0; column < 8; column++)
-			ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthFixed);
+	DrawBoard();
 
-		for (int rank = 0; rank < 8; rank++)
-		{
-			for (int file = 0; file < 8; file++)
-			{
-				int sq = (7 - rank) * 8 + file;
-				bool IsDark = ((rank + file) % 2) == 1;
-				ImU32 cellColor = IsDark ? Colors.darkColor : Colors.lightColor;
-
-				if (SelectedSquare == sq)
-					cellColor = IsDark ? Colors.darkHLColor : Colors.lightHLColor;
-
-				ImGui::TableNextColumn();
-				ImGui::TableSetBgColor(ImGuiTableBgTarget_CellBg, cellColor);
-
-				Piece piece = Board.GetPiece(Square(sq));
-				if ((sq != SelectedSquare || PieceHeld == Piece::NoPiece) && piece != Piece::NoPiece)
-				{
-					std::string image_path = GetPieceImageName(piece);
-					assets::ImageManager::Ptr piece_image = Images.get("Pieces://" + image_path);
-					ImGui::Image(piece_image->ImGuiID(), Cellsize);
-				}
-				else
-				{
-					ImGui::Dummy(Cellsize);
-				}
-
-				bool SquareIsHovered = false;
-				if (ImGui::IsItemHovered())
-				{
-					HoveredSquare = sq;
-					SquareIsHovered = true;
-					any_hovered = true;
-					HandlePieceMoving(SelectedSquare, SelectedAgain, PieceHeld, sq, piece);
-				}
-
-				if (SelectedSquare != -1 && is_legal(Square(SelectedSquare), Square(sq)))
-				{
-					DrawTargetCircle(SquareIsHovered, piece != Piece::NoPiece, CellSide);
-				}
-			}
-		}
-
-		ImGui::EndTable();
-	}
-
-	if (ImGui::IsMouseDown(0) && PieceHeld != Piece::NoPiece)
-		DrawSelectedPiece(PieceHeld);
-
-	ImGui::Text("Square selected: %d", SelectedSquare);
-	ImGui::Text("Selected again: %s", SelectedAgain ? "True" : "False");
-	ImGui::Text("Square hovered: %d", HoveredSquare);
-	ImGui::Text("Legal target: %s", is_legal(Square(SelectedSquare), Square(HoveredSquare)) ? "True" : "False");
-	ImGui::Text("Testing: %I64u", SelectedSquare != -1 ? Chess::Bitboards::LegalTargets[SelectedSquare] : 0);
-	ImGui::Text("Bitboard for square %d: %I64u", HoveredSquare, HoveredSquare != -1 ? Chess::Bitboards::LegalTargets[HoveredSquare] : 0);
-	ImGui::Text("Size: %I64u", sizeof(Chess::Bitboards::LegalTargets));
-	ImGui::Text("Size2: %I64u", sizeof(Chess::Bitboards::PromoMask));
+	HandleMoving();
 
 	auto& moves = Board.GetMoves();
 	ImGui::Text("Number of moves: %d", moves.size());
 
-	if (!any_hovered)
-		HoveredSquare = -1;
+	std::string state_name = GetStateName(State);
+
+	ImGui::Text("Hovered square: %d", HoveredSquare);
+	ImGui::Text("Current state: %s", state_name.c_str());
+
+	if (ImGui::Button("Unmake last move") && !MoveHistory.empty())
+	{
+		Move last_move = MoveHistory.back();
+		MoveHistory.pop_back();
+
+		Board.UnMakeMove(last_move);
+	}
 
 	ImGui::End();
 }
@@ -135,9 +80,163 @@ std::string ChessWindow::GetPieceImageName(Chess_Rework::Piece piece) const
 	return file_name;
 }
 
-void ChessWindow::DrawSelectedPiece(Chess_Rework::Piece piece) const
+void ChessWindow::DrawBoard()
 {
-	std::string image_path = GetPieceImageName(piece);
+	namespace Chess = Chess_Rework;
+	using Chess::Piece, Chess::Square, Chess::Bitboards::is_legal;
+
+	static bool SelectedAgain = false;
+
+	float CellSide = 100.f;
+	ImVec2 CellSize = ImVec2(CellSide, CellSide);
+
+	ImGui::Begin(WindowName.c_str());
+
+	bool MouseOverBoard = false;
+	if (ImGui::BeginTable("Chessboard", 8))
+	{
+		for (int column = 0; column < 8; column++)
+			ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthFixed, CellSide);
+
+		for (int rank = 0; rank < 8; rank++)
+		{
+			ImGui::TableNextRow(0, CellSide);
+			for (int file = 0; file < 8; file++)
+			{
+				int sq = (7 - rank) * 8 + file;	// We draw in reverse order of the ranks
+				bool IsDark = ((rank + file) % 2) == 1;
+				ImU32 cellColor = IsDark ? Colors.darkColor : Colors.lightColor;
+
+				if (SelectedSquare == sq)
+					cellColor = IsDark ? Colors.darkHLColor : Colors.lightHLColor;
+
+				ImGui::TableNextColumn();
+				ImGui::TableSetBgColor(ImGuiTableBgTarget_CellBg, cellColor);
+
+				Piece piece = Board.GetPiece(Square(sq));
+				PopulateCell(sq, CellSize, piece);
+
+				if (ImGui::IsItemHovered())
+				{
+					HoveredSquare = sq;
+					MouseOverBoard = true;
+				}
+
+				if (is_legal(Square(SelectedSquare), Square(sq)))
+					DrawTargetCircle(HoveredSquare == sq, piece != Piece::NoPiece, CellSide);
+
+				if (State == InputState::Promoting && sq == TargetSquare)
+					DrawPromotionWindow(CellSize);
+			}
+		}
+
+		ImGui::EndTable();
+	}
+
+	if (!MouseOverBoard)
+		HoveredSquare = -1;
+}
+
+void ChessWindow::DrawPromotionWindow(ImVec2 CellSize)
+{
+	namespace Chess = Chess_Rework;
+	using Chess::Piece, Chess::PieceType;
+	
+	bool WhiteToPlay = Board.IsWhiteToMove();
+
+	ImVec2 Cursor = ImGui::GetCursorScreenPos();
+	auto flags =	ImGuiWindowFlags_NoCollapse | 
+					ImGuiWindowFlags_NoMove | 
+					ImGuiWindowFlags_NoResize | 
+					ImGuiWindowFlags_NoTitleBar | 
+					ImGuiWindowFlags_NoSavedSettings |
+					ImGuiWindowFlags_NoScrollbar;
+	
+	float WindowSizeX = CellSize.x * 1.08f;
+	float WindowSizeY = CellSize.y * 1.08f;
+	ImVec2 WindowSize = ImVec2(WindowSizeX, WindowSizeY * 4);
+
+	ImVec2 WindowPos = ImVec2(Cursor.x - CellSize.x * 0.04f, Cursor.y - CellSize.y * 1.06f);
+	if (!WhiteToPlay)
+		WindowPos.y -= WindowSize.y - CellSize.x * 1.04f;
+
+	ImU32 White = IM_COL32(255, 255, 255, 255);
+
+	ImGui::SetNextWindowSize(WindowSize);
+	ImGui::SetNextWindowPos(WindowPos);
+	
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.f, 0.f));
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.f);
+	ImGui::PushStyleVar(ImGuiStyleVar_ItemInnerSpacing, ImVec2(0.f, 0.f));
+	ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0.f, 0.f));
+	ImGui::PushStyleVar(ImGuiStyleVar_ImageBorderSize, 0.f);
+	ImGui::PushStyleColor(ImGuiCol_WindowBg, White);
+
+	ImGui::Begin("PromotionWindow", nullptr, flags);
+
+	auto queen = Images.get("Pieces://" + GetPieceImageName(WhiteToPlay ? Piece::W_Queen : Piece::B_Queen));
+	auto rook = Images.get("Pieces://" + GetPieceImageName(WhiteToPlay ? Piece::W_Rook : Piece::B_Rook));
+	auto bishop = Images.get("Pieces://" + GetPieceImageName(WhiteToPlay ? Piece::W_Bishop : Piece::B_Bishop));
+	auto knight = Images.get("Pieces://" + GetPieceImageName(WhiteToPlay ? Piece::W_Knight : Piece::B_Knight));
+
+	if (!ImGui::IsWindowFocused())
+		State = InputState::PromotionCancel;
+
+	float diff = 0.95f;
+	float NewX = WindowSizeX * diff;
+	ImVec2 ImgSize = ImVec2(NewX, NewX);
+
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, White);
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, White);
+	ImGui::PushStyleColor(ImGuiCol_Button, White);
+
+	if (WhiteToPlay)
+	{
+		assets::ImageManager::Ptr imgs[4] = {queen, rook, bishop, knight};
+		PieceType types[4] = {PieceType::Queen, PieceType::Rook, PieceType::Bishop, PieceType::Knight};
+		
+		for (int idx = 0; idx < 4; idx++)
+			if (ImGui::ImageButton(std::format("img##{}", idx).c_str(), imgs[idx]->ImGuiID(), ImgSize))
+				PromotionType = types[idx];
+	}
+	else
+	{
+		assets::ImageManager::Ptr imgs[4] = { knight, bishop, rook, queen };
+		PieceType types[4] = { PieceType::Knight, PieceType::Bishop, PieceType::Rook, PieceType::Queen };
+
+		for (int idx = 0; idx < 4; idx++)
+			if (ImGui::ImageButton(std::format("img##{}", idx).c_str(), imgs[idx]->ImGuiID(), ImgSize))
+				PromotionType = types[idx];
+	}
+	ImGui::PopStyleColor(3);
+
+	ImGui::End();
+	
+	ImGui::PopStyleColor();
+	ImGui::PopStyleVar(5);
+}
+
+void ChessWindow::PopulateCell(int sq, ImVec2 Cellsize, Chess_Rework::Piece piece) const
+{
+	using Chess_Rework::Piece;
+
+	bool SkipSquare = sq == SelectedSquare && (State == InputState::Dragging || State == InputState::Promoting);
+
+	if (!SkipSquare && piece != Piece::NoPiece)
+	{
+		std::string image_path = GetPieceImageName(piece);
+		assets::ImageManager::Ptr piece_image = Images.get("Pieces://" + image_path);
+		ImGui::Image(piece_image->ImGuiID(), Cellsize);
+	}
+	else
+	{
+		ImGui::Dummy(Cellsize);
+	}
+}
+
+void ChessWindow::DrawSelectedPiece() const
+{
+	std::string image_path = GetPieceImageName(PieceHeld);
 	assets::ImageManager::Ptr piece_image = Images.get("Pieces://" + image_path);
 
 	ImVec2 MousePos = ImGui::GetMousePosOnOpeningCurrentPopup();
@@ -218,5 +317,178 @@ void ChessWindow::HandlePieceMoving(int& SelectedSquare, bool& SelectedAgain, Ch
 		if (SelectedAgain && CurrentSq == SelectedSquare) { SelectedSquare = -1; SelectedAgain = false; }
 
 		PieceHeld = Piece::NoPiece;
+	}
+}
+
+void ChessWindow::HandleMoving()
+{
+	switch (State)
+	{
+	case InputState::Idle:
+		HandleMoving_Idle();
+		break;
+	case InputState::Selected:
+		HandleMoving_Selected();
+		break;
+	case InputState::Dragging:
+		HandleMoving_Dragging();
+		break;
+	case InputState::Promoting:
+		HandleMoving_Promotion();
+		break;
+	case InputState::PromotionCancel:
+		HandleMoving_PromotionCancel();
+		break;
+	default: break;
+	}
+}
+
+void ChessWindow::HandleMoving_Idle()
+{
+	namespace Chess = Chess_Rework;
+	using Chess::Square, Chess::Piece;
+
+	if (MouseClicked)
+	{
+		if (auto piece = Board.GetPiece(Square(HoveredSquare)); piece != Piece::NoPiece)
+		{
+			SelectedSquare = HoveredSquare;
+			PieceHeld = piece;
+			State = InputState::Dragging;
+		}
+	}
+}
+
+void ChessWindow::HandleMoving_Dragging()
+{
+	namespace Chess = Chess_Rework;
+	using Chess::Square, Chess::Piece, Chess::Move;
+	using Chess::Bitboards::is_legal, Chess::Bitboards::is_promotion_move;
+
+	DrawSelectedPiece();
+
+	if (MouseReleased)
+	{
+		PieceHeld = Piece::NoPiece;
+		if (is_legal(Square(SelectedSquare), Square(HoveredSquare)))
+		{
+			TargetSquare = HoveredSquare;
+			SelectedAgain = false;
+			if (is_promotion_move(Square(SelectedSquare), Square(TargetSquare)))
+			{
+				State = InputState::Promoting;
+				return;
+			}
+			// Making a move
+			Move move = Board.CreateMove(Square(SelectedSquare), Square(TargetSquare));
+			Board.MakeMove(move);
+			MoveHistory.emplace_back(move);
+
+			SelectedSquare = TargetSquare = -1;
+			State = InputState::Idle;
+			return;
+		}
+		else if (SelectedAgain && SelectedSquare == HoveredSquare)
+		{
+			SelectedSquare = -1;
+			SelectedAgain = false;
+			State = InputState::Idle;
+		}
+		else
+		{
+			State = InputState::Selected;
+		}
+	}
+}
+
+void ChessWindow::HandleMoving_Selected()
+{
+	namespace Chess = Chess_Rework;
+	using Chess::Square, Chess::Piece, Chess::Move;
+	using Chess::Bitboards::is_legal, Chess::Bitboards::is_promotion_move;
+
+	if (MouseClicked)
+	{
+		SelectedAgain = false;
+		if (HoveredSquare == SelectedSquare)
+		{
+			PieceHeld = Board.GetPiece(Square(SelectedSquare));
+			SelectedAgain = true;
+			State = InputState::Dragging;
+			return;
+		}
+		if (is_legal(Square(SelectedSquare), Square(HoveredSquare)))
+		{
+			TargetSquare = HoveredSquare;
+			SelectedAgain = false;
+			if (is_promotion_move(Square(SelectedSquare), Square(TargetSquare)))
+			{
+				State = InputState::Promoting;
+				return;
+			}
+			// Making a move
+			Move move = Board.CreateMove(Square(SelectedSquare), Square(TargetSquare));
+			Board.MakeMove(move);
+			MoveHistory.emplace_back(move);
+
+			SelectedSquare = TargetSquare = -1;
+			State = InputState::Idle;
+			return;
+		}
+
+		if (auto piece = Board.GetPiece(Square(HoveredSquare)); piece != Piece::NoPiece)
+		{
+			PieceHeld = piece;
+			SelectedSquare = HoveredSquare;
+			State = InputState::Dragging;
+		}
+		else
+		{
+			SelectedSquare = -1;
+			State = InputState::Idle;
+		}
+	}
+}
+
+void ChessWindow::HandleMoving_Promotion()
+{
+	namespace Chess = Chess_Rework;
+	using Chess::Square, Chess::PieceType, Chess::Move;
+
+	if (PromotionType != PieceType::NoPieceType)
+	{		
+		Move move = Board.CreateMove(Square(SelectedSquare), Square(TargetSquare), PromotionType);
+		Board.MakeMove(move);
+		MoveHistory.emplace_back(move);
+
+		PromotionType = PieceType::NoPieceType;
+		SelectedSquare = TargetSquare = -1;
+		State = InputState::Idle;
+	}
+}
+
+void ChessWindow::HandleMoving_PromotionCancel()
+{
+	PromotionType = Chess_Rework::PieceType::NoPieceType;
+	SelectedSquare = TargetSquare = -1;
+	State = InputState::Idle;
+}
+
+const char* ChessWindow::GetStateName(InputState state) const
+{
+	switch (state)
+	{
+	case ChessWindow::InputState::Idle:
+		return "Idle";
+	case ChessWindow::InputState::Selected:
+		return "Selected";
+	case ChessWindow::InputState::Dragging:
+		return "Dragging";
+	case ChessWindow::InputState::Promoting:
+		return "Promoting";
+	case ChessWindow::InputState::PromotionCancel:
+		return "PromotionCancel";
+	default:
+		return "Unknonw";
 	}
 }
--- ChessManager/Rework/Windows/ChessWindow.h
+++ ChessManager/Rework/Windows/ChessWindow.h
@@ -3,7 +3,7 @@
 #include "Chess/types.h"
 #include "../asset_manager.hpp"
 #include <string>
-
+#include <vector>
 
 class ChessWindow : public Window
 {
@@ -12,6 +12,8 @@ public:
 	void Draw();
 
 private:
+	enum class InputState { Idle, Selected, Dragging, Promoting, PromotionCancel };
+	
 	struct WindowColors
 	{
 		ImU32 lightColor = IM_COL32(235, 236, 208, 255);
@@ -26,14 +28,41 @@ private:
 
 private:
 	std::string GetPieceImageName(Chess_Rework::Piece piece) const;
-	void DrawSelectedPiece(Chess_Rework::Piece piece) const;
+	
+	void DrawBoard();
+	void PopulateCell(int sq, ImVec2 Cellsize, Chess_Rework::Piece piece) const;
+	void DrawSelectedPiece() const;
 	void DrawTargetCircle(bool IsHovered, bool PieceOnSquare, float CellSize) const;
+	void DrawPromotionWindow(ImVec2 CellSize);
+
 	bool AttemptMakeMove(int sq1, int sq2);
 	void HandlePieceMoving(int& SelectedSquare, bool& SelectedAgain, Chess_Rework::Piece& PieceHeld, int CurrentSq, Chess_Rework::Piece PieceOnSq);
 
+	void HandleMoving();
+	void HandleMoving_Idle();
+	void HandleMoving_Dragging();
+	void HandleMoving_Selected();
+	void HandleMoving_Promotion();
+	void HandleMoving_PromotionCancel();
+	const char* GetStateName(InputState state) const;
+
 private:
+	std::vector<Chess_Rework::Move> MoveHistory;
+
 	WindowColors Colors;
 
 	Chess_Rework::Chessboard_New& Board;
 	assets::ImageManager& Images;
+
+	InputState State = InputState::Idle;
+
+	int SelectedSquare = -1;
+	int HoveredSquare = -1;
+	int TargetSquare = -1;
+	Chess_Rework::Piece PieceHeld = Chess_Rework::Piece::NoPiece;
+	Chess_Rework::PieceType PromotionType = Chess_Rework::PieceType::NoPieceType;
+	bool SelectedAgain	= false;
+	bool MouseClicked	= false;
+	bool MouseReleased	= false;
+	bool LeftMouseDown	= false;
 };
